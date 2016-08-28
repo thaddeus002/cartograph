d719a1b72fc935d168bc36df285d799514d8e391
thaddeus002 <thaddeus.dupont@free.fr>
2016-08-28 19:18:28 +0200

    Adding bln data module

---
+++ src/bln.c
@@ -0,0 +1,176 @@
+/**
+ * \file bln.c
+ */
+
+#include "bln.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#define LINE_LENGHT 250
+
+/**
+ * \brief Read a bln file.
+ * \param filename
+ * \return the content of the file
+ */
+bln_data_t *bln_read_file(char *filename){
+
+    bln_data_t *result;
+    FILE *fd;
+    int i=0; // line number
+    char buf_read[LINE_LENGHT]; /* one file's line */
+    bln_data_t *current = NULL; // the next struct to add to result
+
+
+    fd=fopen(filename, "r");
+    if(fd == NULL) {
+        fprintf(stderr, "Unable to read file %s\n", filename);
+        return NULL;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /* Lecture ligne par ligne */
+    while(fgets(buf_read, LINE_LENGHT, fd)!=NULL){
+
+        char X[20], Y[20]; // to read integers or floats
+
+        if(i==0) { /* ligne d'entete */
+
+            char pays[LINE_LENGHT];
+            char region[LINE_LENGHT];
+
+            current = malloc(sizeof(bln_data_t));
+
+            sscanf(buf_read, "%d,%d,%[a-zA-Z'\" -],%s", &(current->nbPoints), &(current->closed), pays, region);
+            if (current->nbPoints<=0) {
+                free(current);
+                continue; /* i reste à 0 pour la lecture de la prochaine ligne */
+            } else {
+                current->name = malloc(sizeof(char) * (strlen(pays)+1));
+                strcpy(current->name, pays);
+                current->description = malloc(sizeof(char) * (strlen(region)+1));
+                strcpy(current->description, region);
+                current->x = malloc(sizeof(float)*current->nbPoints);
+                current->y = malloc(sizeof(float)*current->nbPoints);
+                current->next = NULL;
+            }
+        }
+
+        else { /* ligne de donnée : coordonnée d'un point */
+
+            /* lecture d'un enregistrement */
+            if(sscanf(buf_read, "%[0-9.-],%[0-9.-]", X, Y)==0) {
+                bln_destroy(current);
+                bln_destroy(result);
+                fclose(fd);
+                fprintf(stderr, "Error while reading bln file : %s\n", buf_read);
+                return NULL;
+            }
+            current->x[i-1]=atof(X);
+            current->y[i-1]=atof(Y);
+
+        }
+
+
+
+        i++;
+
+        // Validate data
+        if(i>current->nbPoints) {
+
+            if(result == NULL) result = current;
+            else {
+                bln_data_t *last = result;
+                while (last->next != NULL) last = last->next;
+                last->next = current;
+            }
+
+            i=0;
+        }
+    } //reached end of file
+
+    if(i<=current->nbPoints) bln_destroy(current);
+
+    return result;
+}
+
+
+/**
+ * \brief free the memory used by the bln data.
+ */
+void bln_destroy(bln_data_t *data){
+
+    bln_data_t *current = data;
+
+    while(current != NULL) {
+
+        bln_data_t *next;
+
+        if(current->name != NULL) free(current->name);
+        if(current->description != NULL) free(current->description);
+        if(current->x != NULL) free(current->x);
+        if(current->y != NULL) free(current->y);
+
+        next = current->next;
+        free(current);
+        current=next;
+    }
+}
+
+
+/**
+ * \brief Write the data in a file.
+ */
+int bln_write_file(bln_data_t *data, char *filename){
+
+    bln_data_t *current;
+    FILE *fd;
+
+    fd=fopen(filename, "w");
+    if(fd == NULL) {
+        fprintf(stderr, "Unable to create file %s\n", filename);
+        return -1;
+    }
+
+    current = data;
+
+    while(current!=NULL) {
+
+        int closed = 0;
+        int i;
+        char *name = "";
+        char *desc = "";
+
+        if(current->closed) closed=1;
+        if(current->name !=NULL) name = current->name;
+        if(current->description != NULL) desc = current->description;
+
+        fprintf(fd, "%d,%d,\"%s\",\"%s\"\n", current->nbPoints, closed, name, desc);
+
+        for(i=1; i<=current->nbPoints; i++) {
+
+            fprintf(fd, "%f,%f\n", current->x[i-1], current->y[i-1]);
+        }
+
+        current = current->next;
+    }
+
+    fclose(fd);
+    return 0;
+}
+
+
+
---
+++ src/bln.h
@@ -0,0 +1,60 @@
+/**
+ * \file bln.h
+ * \brief A BLN file contains informations to draw a line on a map.
+ *
+ * Each line of file contains the coordinates of a point. Linking the points
+ * give à curve : country boundaries, rivers, roads, ...
+ */
+
+#ifndef __BLN_FILE_H__
+#define __BLN_FILE_H__ 1
+
+
+/**
+ * \brief Information a of line. A BLN file may contains many of lines. So this
+ * struct may be linked.
+ *
+ * The first line contains 1 to 4 informations :
+ *    - Number of points
+ *    - Closed (optionnal) : 0 or 1, indicate if the line is closed (i.e. the last
+ * point is the same as the first)
+ *    - Name (optional) : for a country this is the name of the country
+ *    - Description (optional) : for exemple it can be a region's name
+ * Then there are nbPoints line with lon,lat coordinates (it can be x,y
+ * in Lambert's coordinates)
+ */
+typedef struct bln_data_t_ {
+    int nbPoints; /**< \brief number of points */
+    int closed; /**< \brief not NULL if the line is closed */
+    char *name; /**< \brief line name, can be a country name */
+    char *description; /**< \brief complementary description, may be a region */
+    /** tables of coordinates */
+    float *x, *y;
+    struct bln_data_t_ *next; /**< \brief link to the next structur */
+} bln_data_t;
+
+
+
+/**
+ * \brief Read a bln file.
+ * \param filename
+ * \return the content of the file
+ */
+bln_data_t *bln_read_file(char *filename);
+
+
+/**
+ * \brief free the memory used by the bln data.
+ */
+void bln_destroy(bln_data_t *data);
+
+
+/**
+ * \brief Write the data in a file.
+ */
+int bln_write_file(bln_data_t *data, char *filename);
+
+
+
+
+#endif
