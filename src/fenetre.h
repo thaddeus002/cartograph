821d67735c37e765e1996dbd62a427ee74eadbac
thaddeus002 <thaddeus.dupont@free.fr>
2017-09-15 23:55:04 +0200

    Improves API's documentation

--- src/fenetre.h
+++ src/fenetre.h
@@ -1,6 +1,6 @@
 /**
  * \file fenetre.h
- * Gestion de la fenetre et des principales fcts de la xlib
+ * Management a window and the main functions of the xlib.
  */
 
 
@@ -24,13 +24,16 @@
 #include "outils.h"
 
 
-/* variables du XSystem */
-Pixel *pix; // tous les pixels avec les couleurs utiles - utilisation de pix pour la grille de relief :
-//affichage.c:82:                   pointe_pixel(f, xlamb, ylamb, /*0*/largeur, pix[n+12000], CARRE);
+/* XSystem variables */
 
-#define PLAGE 12000 /* nb max de pixels dans le tableau pix */
+/** Array of the pixels with usefull color. Is used in relief grid. */
+Pixel *pix;
 
-typedef enum{NOIR=0, BLANC, ROUGE, VERT, BLEU, ORANGE, JAUNE, CYAN, MAGENTA, MARRON} couleur;
+/** Max number of pixels in 'pix' array */
+#define PLAGE 12000
+
+typedef enum {NOIR=0, BLANC, ROUGE, VERT, BLEU, ORANGE, JAUNE, CYAN, MAGENTA, MARRON} couleur;
+
 //Pixel rouge, vert, bleu, orange, jaune, cyan, magenta; // red, green blue deja utilisé
 Pixel pix_colore[10];
 
@@ -41,8 +44,10 @@ typedef struct {
     GC gc;
     Visual *visual;
 
-    float x1, x2, y1, y2; /* les coordonnées planes (Lambert ou autres) correspondant aux bornes de la fenetre */
-    int w, h; /* taille de la fenetre en pixels */
+    /* flat coordinates (may be Lambert) of the window's bounds */
+    float x1, x2, y1, y2;
+    /* window's size in pixels */
+    int w, h;
 } fenetre;
 
 
@@ -54,16 +59,22 @@ void init_colors(char *range, int depth, int gamma_f, int hmax);
 
 
 /* retourne w=0 si échec */
-fenetre cree_fenetre(int width, int height, int *depth, float x1, float x2, float y1, float y2/*,  XGCValues *gcv*/);
+fenetre cree_fenetre(int width, int height, int *depth, float x1, float x2, float y1, float y2);
 /* retourne w=0 si echec */
 fenetre cree_fenetre_coloree(int width, int height, int *depth, float x1, float x2, float y1, float y2, couleur c);
 
 
-/* gère les évènements clavier et souris */
+
+/**
+ * Manage mouse and keyboard events.
+ * \param fen the window
+ * \param event the event which occured
+ */
 int manage_event(fenetre fen, XEvent event);
 
 
-/* fonctions de tracé */
+/* Drawing functions */
+
 /* devraient fctionner en coordonnées lambert -> à modifier */
 void do_output_pixel(fenetre f, int u, int v, Pixel p);
 void do_output(fenetre f, int u, int v, couleur n);
@@ -71,35 +82,80 @@ void do_output(fenetre f, int u, int v, couleur n);
 /* fctionne en coordonnées Lambert */
 void trace_ligne(fenetre f, float u1, float u2, float v1, float v2, couleur n, int largeur);
 
-/* fctionnent en coordonnées fenetres */
+
+/**
+ * Draw lignes in window's coordinates.
+ * \param f where to draw
+ * \param points array of points defining a multiline curve
+ * \param nb number of points in the array 'points'
+ * \param n the lines' color
+ * \param largeur lines' width
+ */
 void trace_lignes(fenetre f, XPoint *points, int nb, couleur n, int largeur);
-/* remplissage d'un polygone */
+
+
+/**
+ * Fill a polygon.
+ * \param f where to draw
+ * \param points the polygon's corners
+ * \param nb number of points
+ * \param n color for the inner polygon
+ */
 void remplit(fenetre f, XPoint *points, int nb, couleur n);
 
 
-/* écrit du texte dans la fenêtre */
+/**
+ * \brief Draw a text in a window.
+ * \param f where to draw
+ * \param x x coordinate of the beginning point
+ * \param y y coodinate of the beginning point
+ * \param text the text to print
+ * \param color text's color
+ */
 void display_text(fenetre f, int x, int y, char * text, couleur c);
+
+
+/**
+ * \brief Draw a text in a window in white.
+ *
+ * Is equivantent to : display_text(f,x,y,text,BLANC).
+ * \param f where to draw
+ * \param x x coordinate of the beginning point
+ * \param y y coodinate of the beginning point
+ * \param text the text to print
+ */
 void display_text_blanc(fenetre f, int x, int y, char * text);
 
 
-
-/*trace un point en coordonnées fenêtre */
+/**
+ * Draw a point in window's coordinates.
+ */
 int trace_point(fenetre f, int x, int y, couleur c);
-/* trace un point en fct des coordonnées Lambert */
+
+
+/**
+ * Draw a point in Lambert's coordinates.
+ */
 int pointe_pixel(fenetre f, float xLamb, float yLamb, int L, Pixel p, forme_t forme);
+
 int pointe(fenetre f, float xLamb, float yLamb, int L, couleur c, forme_t forme);
 
 void trace_echelle(fenetre fen, couleur cdessin, couleur ctexte);
 
 
-/* affichage dans la fenetre des modifications faites sur le pixmap par les fcts précédentes */
+/**
+ * Update/refresh window's view.
+ */
 void actualise_fenetre(fenetre fen);
 
-/* fermeture des display */
+
+/**
+ * Closes the displays.
+ */
 int fermeture(fenetre fen);
 
+/* Save the window's drawing in files */
 
-/* enregistre le dessin d'une fenetre dans un fichier */
 void sauve_fenetre_jpeg(fenetre *f, char *filename);
 void sauve_fenetre_png(fenetre *f, char *filename);
 void sauve_fenetre_tiff(fenetre *f, char *filename);
