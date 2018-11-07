3297d57e6f11ecb26ff6bfbceab7fa535494a1c8
thaddeus002 <thaddeus.dupont@free.fr>
2018-11-07 05:41:22 +0100

    refactor (lecture_bln.c) :
    remove unused include and constant
    add a return statement to non-void function
    format text

--- src/lecture_bln.c
+++ src/lecture_bln.c
@@ -1,17 +1,75 @@
-/** \file lecture_bln.c */
-/* fonctions de tracé de données de contours présentes dans un fichier au
-** format BLN */
-
+/**
+ * \file lecture_bln.c
+ * Draw BLN data.
+ */
 
 #include "lecture_bln.h"
 #include "coordonnees.h"
-#include "outils.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 
-#define LIGNE 250 /* longueur maximale des lignes du fichier, au dela on n'en tient pas compte */
 
+/**
+ * The drawing function.
+ * 
+ * Shows bln data in a window.
+ * \param data the bln data to draw
+ * \param f the window where draw
+ * \param largeur lines' width
+ * \param c lines' color
+ * \param ligne 1 if the points must be joined by lines
+ * \param remplir 1 if the closed forms must be filled
+ * \param remplissage filling color
+ * \return 0 on success or an error code
+ */
+static int bln_show_in_window_lines(bln_data_t *data, fenetre f, int largeur, couleur c, int ligne, int remplir, couleur remplissage) {
+
+    bln_data_t *current = data;
+    while(current != NULL) {
+
+        int n = 0; // point index
+        float Xm,Ym; // where show the countries' names
+
+        if(ligne == 0) {
+            while(n<current->nbPoints) {
+                pointe(f,current->x[n],current->y[n],largeur,c,CARRE);
+                n++;
+            }
+        } else {
+            if(remplir && current->x[0]==current->x[current->nbPoints-1] && current->y[0]==current->y[current->nbPoints-1] ) {
+                window_fill_polygon(f, current->x, current->y, current->nbPoints, remplissage);
+            }
+            window_draw_lines(f, current->x, current->y, current->nbPoints, c, largeur);
+        }
+
+        Xm=0.5*(current->xmin+current->xmax); Ym=0.5*(current->ymin+current->ymax);
+
+        if((current->name!=NULL)&&(current->nbPoints>=2)) {
+            display_text(f,xpoint(&f, Xm)-(strlen(current->name))*4,ypoint(&f, Ym),current->name,JAUNE);
+        }
+
+        current = current->next; 
+    }
+    return 0;
+}
+
+
+/**
+ * The drawing function.
+ * 
+ * Shows bln data in a window.
+ * \param data the bln data to draw
+ * \param f the window where draw
+ * \param largeur lines' width
+ * \param c lines' color
+ * \param remplir 1 if the closed forms must be filled
+ * \param remplissage filling color
+ * \return 0 on success or an error code
+ */
+int bln_show_in_window(bln_data_t *data, fenetre f, int largeur, couleur c, int remplir, couleur remplissage) {
+    return bln_show_in_window_lines(data, f, largeur, c, 1, remplir, remplissage);
+}
 
 
 /**
@@ -82,71 +140,6 @@ static void lambInHm(bln_data_t *data) {
 }
 
 
-
-/**
- * The drawing function.
- * 
- * Shows bln data in a window.
- * \param data the bln data to draw
- * \param f the window where draw
- * \param largeur lines' width
- * \param c lines' color
- * \param ligne 1 if the points must be joined by lines
- * \param remplir 1 if the closed forms must be filled
- * \param remplissage filling color
- * \return 0 on success or an error code
- */
-static int bln_show_in_window_lines(bln_data_t *data, fenetre f, int largeur, couleur c, int ligne, int remplir, couleur remplissage) {
-
-    bln_data_t *current = data;
-    while(current != NULL) {
-
-        int n = 0; // point index
-        float Xm,Ym; // where show the countries' names
-
-        if(ligne == 0) {
-            while(n<current->nbPoints) {
-                pointe(f,current->x[n],current->y[n],largeur,c,CARRE);
-                n++;
-            }
-        } else {
-            if(remplir && current->x[0]==current->x[current->nbPoints-1] && current->y[0]==current->y[current->nbPoints-1] ) {
-                window_fill_polygon(f, current->x, current->y, current->nbPoints, remplissage);
-            }
-            window_draw_lines(f, current->x, current->y, current->nbPoints, c, largeur);
-        }
-
-        Xm=0.5*(current->xmin+current->xmax); Ym=0.5*(current->ymin+current->ymax);
-
-        if((current->name!=NULL)&&(current->nbPoints>=2)) {
-            display_text(f,xpoint(&f, Xm)-(strlen(current->name))*4,ypoint(&f, Ym),current->name,JAUNE);
-        }
-
-        current = current->next; 
-    }
-}
-
-
-
-/**
- * The drawing function.
- * 
- * Shows bln data in a window.
- * \param data the bln data to draw
- * \param f the window where draw
- * \param largeur lines' width
- * \param c lines' color
- * \param remplir 1 if the closed forms must be filled
- * \param remplissage filling color
- * \return 0 on success or an error code
- */
-int bln_show_in_window(bln_data_t *data, fenetre f, int largeur, couleur c, int remplir, couleur remplissage) {
-    return bln_show_in_window_lines(data, f, largeur, c, 1, remplir, remplissage);
-}
-
-
-
-
 /**
  * The drawing function.
  * 
@@ -184,15 +177,13 @@ static int trace_bln_V2(char *filename, fenetre f, int largeur, couleur c, int l
 /* passer un nom de fichier bln en paramètre et on s'occupe de tout */
 /* retourne un code d'erreur non nul en cas d'échec */
 int trace_bln_points(char *fichier, fenetre f, int largeur, couleur c, int remplir, couleur remplissage){
-
-    return(trace_bln_V2(fichier,f, largeur, c,0, 0, remplir, remplissage));
+    return(trace_bln_V2(fichier, f, largeur, c, 0, 0, remplir, remplissage));
 }
 
 /* passer un nom de fichier bln en paramètre et on s'occupe de tout */
 /* retourne un code d'erreur non nul en cas d'échec */
 int trace_bln_lignes(char *fichier, fenetre f, int largeur, couleur c, int remplir, couleur remplissage){
-
-    return(trace_bln_V2(fichier, f, largeur, c,1 , 0, remplir, remplissage));
+    return(trace_bln_V2(fichier, f, largeur, c, 1 , 0, remplir, remplissage));
 }
 
 
@@ -200,6 +191,6 @@ int trace_bln_lignes(char *fichier, fenetre f, int largeur, couleur c, int rempl
 /* retourne un code d'erreur non nul en cas d'échec */
 /* le fichier d'entrée contient des coordonnées en lon et lat */
 int trace_bln_geo(char *fichier, fenetre f, int largeur, couleur c, int remplir, couleur remplissage){
-    return(trace_bln_V2(fichier, f, largeur, c,1 , 1, remplir, remplissage));
+    return(trace_bln_V2(fichier, f, largeur, c, 1 , 1, remplir, remplissage));
 }
 
